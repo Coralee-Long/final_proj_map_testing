e253e0c251d09d57284f49be7d4308a49f70c8cc
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2022-02-21 22:02:59 +0100

    Only use LeafletMap.js & GeoCodeTest.js component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import LeafletMap from "./components/LeafletMap.js";
 import "./Map.css";
 import Data from "./data/germanCities.json";
 import { useState } from "react";
+import GeoCodeTest from "./components/GeoCodeTest.js";
 
 function App() {
   const [locations, setLocations] = useState(Data);
   return (
     <>
+      <GeoCodeTest locations={locations} setLocations={setLocations} />
       <LeafletMap locations={locations} setLocations={setLocations} />
     </>
   );
